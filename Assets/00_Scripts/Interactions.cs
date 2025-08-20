d5866c01b0520749ffa8a89f1e28d24a3c238c04
jroapuniacc <jorge.roap@uniacc.edu>
2025-08-20 17:25:34 -0400

    Clase02

--- Assets/00_Scripts/Interactions.cs
+++ Assets/00_Scripts/Interactions.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class Interactions : MonoBehaviour
 {
     [SerializeField] private bool key1 = false;
+    [SerializeField] private bool key2 = false;
+    [SerializeField] private GameObject door;
+    private Animator animatorDoor;
+
+    private void Start()
+    {
+        animatorDoor = door.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("NPC1"))
+        switch (other.tag)
         {
-            key1 = true;
+            case "NPC1":
+                Debug.Log("Obtuve la llave 1.. al fin");
+                key1 = true;
+                break;
+            case "NPC2":
+                Debug.Log("Obtuve la llave 2.. al fin");
+                key2 = true;
+                break;
+            case "Door":
+                Debug.Log("Abrí la puerta");
+                animatorDoor.SetBool("Anim_Door", true);
+                break;
+            default:
+                Debug.Log("Anda, buscas las llaves");
+                break;
         }
+        
+       
+       
     }
+    
 }
 
 
